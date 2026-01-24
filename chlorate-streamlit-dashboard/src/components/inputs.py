9fa2b790a8eee1926d21b09d35cd42526a7f9c96
Iwan Steele <iwansteele@gmail.com>
2026-01-24 17:17:14 +0000

    Update inputs.py

--- chlorate-streamlit-dashboard/src/components/inputs.py
+++ chlorate-streamlit-dashboard/src/components/inputs.py
@@ -10,7 +10,7 @@ def create_inputs():
         step=0.1,
         format="%.2f"
     )
-    initial_chlorate_g_l = st.sidebar.slider("Initial chlorate in product (g/L)", min_value=0.0, value=0.01, max_value=1.0, step=0.001, format="%.4f")
+    initial_chlorate_g_l = st.sidebar.slider("Initial chlorate in product (g/L)", min_value=0.0, value=0.01, max_value=10.0, step=0.001, format="%.4f")
     chlorine_dose_mg_l = st.sidebar.slider("Chlorine dose into water (mg/L)", min_value=0.0, value=5.0, max_value=20.0, step=0.1)
     pump_max_l_hr = st.sidebar.slider("Dosing pump max (L/hr)", min_value=0.1, value=10.0, max_value=100.0, step=0.1)
     water_flow_MLD = st.sidebar.slider("Water flow (MLD)", min_value=0.1, value=500.0, max_value=500.0, step=1.0, format="%.3f")
